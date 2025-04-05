51482ceacc3a35a82c513c7b43893a0854c84b97
Soumyadip Adak <149286607+adak99@users.noreply.github.com>
2025-04-05 10:48:02 +0530

    Multhreading in java

---
+++ Multithreading/src/Demo2/Test.java
@@ -0,0 +1,14 @@
+package Demo2;
+
+public class Test extends Thread{
+
+    @Override
+    public void run() {
+        
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+    }
+}
