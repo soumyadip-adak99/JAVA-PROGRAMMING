1574060006928b24a9a44b12f86c879e8477303f
Soumyadip Adak <149286607+adak99@users.noreply.github.com>
2025-04-26 13:21:17 +0530

    Update

--- Backtracking/Permutation.java
+++ Backtracking/Permutation.java
@@ -1,23 +1,28 @@
-//Arrange them in a single line "ABC"
+import java.util.List;
+import java.util.ArrayList;
 
 public class Permutation {
+    public static void main(String[] args) {
+        String str = "ABC";
+        System.out.println(permutation(str));
+    }
 
-    public static void printPermutaion(String str, String permutation, int index) {
-        // base case
-        if (str.length() == 0) {
-            System.out.println(permutation);
+    public static List<String> permutation(String str) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", str);
+        return result;
+    }
+
+    public static void backtrack(List<String> result, String temp, String str) {
+        if (str.isEmpty()) {
+            result.add(temp);
             return;
         }
 
         for (int i = 0; i < str.length(); i++) {
             char currentChar = str.charAt(i);
             String newString = str.substring(0, i) + str.substring(i + 1);
-            printPermutaion(newString, permutation + currentChar, index + 1);
+            backtrack(result, temp + currentChar, newString);
         }
-    }
-
-    public static void main(String[] args) {
-        String str = "ABC";
-        printPermutaion(str, "", 0);
     }
 }
