37817221bb0be61c3d87831b3efea0f1fb2973d1
unknown <soumya.dip.adak@outlook.com>
2025-12-23 21:15:39 +0530

    update

---
+++ DSA/src/Tree/BinarySearchTree/BuildBST.java
@@ -0,0 +1,48 @@
+package Tree.BinarySearchTree;
+
+public class BuildBST {
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    public static Node insert(Node root, int value) {
+        if (root == null) {
+            return new Node(value);
+        }
+
+        if (value < root.data) {
+            root.left = insert(root.left, value);
+        }
+
+        if (value > root.data) {
+            root.right = insert(root.right, value);
+        }
+
+        return root;
+    }
+
+    public static void inorderTraversal(Node root) {
+        if (root == null) return;
+
+        inorderTraversal(root.left);
+        System.out.print(root.data + " ");
+        inorderTraversal(root.right);
+    }
+
+    public static void main(String[] args) {
+        int[] values = {5, 1, 3, 4, 2, 7};
+        Node root = null;
+
+        for (int k : values) {
+            root = insert(root, k);
+        }
+
+        inorderTraversal(root);
+    }
+}
