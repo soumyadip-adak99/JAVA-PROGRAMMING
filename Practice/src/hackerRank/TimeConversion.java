bf98445ecfe2066000b1a37ccb9509b978b03411
Soumya ! <soumya.dip.adak@gmail.com>
2026-08-11 22:57:29 +0530

    Time conversion in hacker rank

---
+++ Practice/src/hackerRank/TimeConversion.java
@@ -0,0 +1,26 @@
+// question -> https://www.hackerrank.com/challenges/time-conversion/problem?isFullScreen=true
+package hackerRank;
+
+public class TimeConversion {
+    public static void main(String[] args) {
+        String time = "07:05:45PM";
+        System.out.println(timeConversion(time));
+    }
+
+    static String timeConversion(String time) {
+        String period = time.substring(8);
+        int hour = Integer.parseInt(time.substring(0, 2));
+
+        if (period.equals("AM")) {
+            if (hour == 12) {
+                hour = 0;
+            }
+        } else {
+            if (hour != 12) {
+                hour += 12;
+            }
+        }
+
+        return String.format("%02d%s", hour, time.substring(2, 8));
+    }
+}
